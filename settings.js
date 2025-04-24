c3fea6bcef026eaeebe1c57c1911ce768db85ec4
Knightbotx <vincentobi191@gmail.com>
2025-04-24 08:26:22 +0100

    Update settings.js
    To change number

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '91702395151X', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2349162520865', //et your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
