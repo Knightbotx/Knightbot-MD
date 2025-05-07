ba2c71ffb21e9093a49d2e89905778e17d037c82
Knightbotx <vincentobi191@gmail.com>
2025-05-07 06:32:19 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,8 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '919876543210', //+2349162520865
+  Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
